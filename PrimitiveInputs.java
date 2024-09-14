0a96b7f943e5dcf4c214d79a9473f596bb6a4140
shifashaikh <shifajs10@gmail.com>
2024-09-14 00:08:19 -0700

    Primitives

---
+++ PrimitiveInputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PrimitiveInputs {
+    public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+    int rollno=input.nextInt();
+    System.out.println("my roll no is:"+rollno);
+    }
+}
+// basic of input and output
