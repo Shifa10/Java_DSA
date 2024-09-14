58a5e6fbaab4055f420083c7ac262d314e776629
shifashaikh <shifajs10@gmail.com>
2024-09-14 00:11:34 -0700

    P

---
+++ Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno=23456788;
+        float salary=10000456677.23f;
+        char letter='A';
+        double number=12334567878.20d;
+        System.out.println(rollno);
+        System.out.println(salary);
+        System.out.println(letter);
+        System.out.println(number);
+        
+    }
+}
