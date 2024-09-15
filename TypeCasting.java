75bfd7a721e5a4108794cfa111849a932c7e0aea
shifashaikh <shifajs10@gmail.com>
2024-09-15 07:30:10 -0700

    casting

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // float num=sc.nextFloat();
+        // int num=sc.nextInt();
+        // System.out.println(num);
+
+      int num=(int)(45.54f);
+      System.out.println(num);
+    }
+
+//TypeCasting converting smaller datatype into large datatype and larger into smaller.
+
+    
+}
