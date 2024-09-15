164b6c1728f0e490db9d6bdc0d292cf492df0a99
shifashaikh <shifajs10@gmail.com>
2024-09-15 05:58:26 -0700

    condition

---
+++ If1.java
@@ -0,0 +1,17 @@
+public class If1 {
+    public static void main(String[] args) {
+    
+            int salary=20000;
+            if(salary>10000){
+                salary+=10000;
+            }
+            else{
+                salary+=1000;
+
+
+            }
+            System.out.println(salary);
+        
+    }
+    
+}
