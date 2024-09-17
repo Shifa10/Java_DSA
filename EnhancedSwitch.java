d8150edbc42a8b96e8b58f22d2da4f257cfc3a91
shifashaikh <shifajs10@gmail.com>
2024-09-17 10:50:02 -0700

    direct way to switching

--- EnhancedSwitch.java
+++ EnhancedSwitch.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EnhancedSwitch {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int day=sc.nextInt();
+
+        switch(day){
+
+            case 1,2,3,4,5 -> System.out.println("weekdays");
+
+            case 6,7 -> System.out.println("weekend");
+        }
+    }
 }
