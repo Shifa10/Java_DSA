79f0ff0b2953c08712ed227ab686f3d98110619b
shifashaikh <shifajs10@gmail.com>
2024-09-19 09:35:54 -0700

    function call

---
+++ StringExample.java
@@ -0,0 +1,15 @@
+public class StringExample {
+    public static void main(String[] args) {
+
+        String message=greet();
+        System.out.println(message);
+
+    }
+
+    static String greet(){
+        String greeting="hiii";
+        return greeting;
+    }
+
+}
+
